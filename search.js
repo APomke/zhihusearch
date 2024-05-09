34a42cbfea7cede7c96d541ec82ca77b70f14f66
APomke <3112520587@qq.com>
2024-05-09 15:48:00 +0800

    修复无法二次请求的bug

--- search.js
+++ search.js
@@ -5,7 +5,6 @@ const app = express();
 
 app.use(cors());
 
-const chunks = [];
 // 创建一个空的 JSON 对象
 const NewJsondata = {
     "data": []
@@ -23,7 +22,7 @@ function generateArticle(title, excerpt, title_image, url, content, created) {
 }
 
 // 递归函数来获取专栏文章
-function getArticles(columnId, limit, offset, callback) {
+function getArticles(columnId, limit, offset, chunks, callback) {
     const path = `/api/v4/columns/${columnId}/items?limit=${limit}&offset=${offset}`;
     console.log("Request URL: " + path);
 
@@ -52,10 +51,10 @@ function getArticles(columnId, limit, offset, callback) {
 
             if (currentCount != 0) {
                 // 继续递归调用获取下一批文章
-                getArticles(columnId, limit, parseInt(offset) + currentCount, callback);
+                getArticles(columnId, limit, parseInt(offset) + currentCount, chunks, callback);
             } else {
                 // 已经获取完所有文章，调用回调函数返回结果
-                callback();
+                callback(chunks);
             }
         });
     });
@@ -71,28 +70,27 @@ function getArticles(columnId, limit, offset, callback) {
 // 获取专栏文章总数
 app.get('/api/getTotals', (req, res) => {
     const { columnId, limit, offset } = req.query;
-    getArticles(columnId, limit, offset, (error) => {
-        if (error) {
-            res.status(500).send('Internal Server Error');
-        } else {
-            // 检查是否已经发送了响应
-            if (!res.headersSent) {
-                // 数据格式化只返回文章标题，图片，链接，介绍
-                for(var i = 0; i < chunks.length; i++){
-                    jsonData = JSON.parse(chunks[i]);
-                    for(var j = 0; j < jsonData.data.length; j++){
-                        var title = jsonData.data[j].title;
-                        var excerpt = jsonData.data[j].excerpt;
-                        var title_image = jsonData.data[j].title_image;
-                        var url = jsonData.data[j].url;
-                        var content = jsonData.data[j].content;
-                        var created = jsonData.data[j].created;
-                        const obj = generateArticle(title,excerpt,title_image,url,content,created);
-                        NewJsondata.data.push(obj);
-                    }
+    // 清空 NewJsondata.data
+    NewJsondata.data = [];
+    getArticles(columnId, limit, offset, [], (chunks) => {
+        // 检查是否已经发送了响应
+        if (!res.headersSent) {
+            // 数据格式化只返回文章标题，图片，链接，介绍
+            for (var i = 0; i < chunks.length; i++) {
+                const jsonData = JSON.parse(chunks[i]);
+                for (var j = 0; j < jsonData.data.length; j++) {
+                    const articleData = jsonData.data[j];
+                    var title = articleData.title;
+                    var excerpt = articleData.excerpt;
+                    var title_image = articleData.title_image;
+                    var url = articleData.url;
+                    var content = articleData.content;
+                    var created = articleData.created;
+                    const obj = generateArticle(title, excerpt, title_image, url, content, created);
+                    NewJsondata.data.push(obj);
                 }
-                res.send(NewJsondata);
             }
+            res.send(NewJsondata);
         }
     });
 });
